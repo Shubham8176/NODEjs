0038654aba6c3cde3b2ff8a2880c2066c7f96576
Shubham8176 <shubhamkumargupta8176sg@gmail.com>
2026-02-12 15:18:41 +0530

    1

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const http=require('http');
-const { CLIENT_RENEG_LIMIT } = require('tls');
-const myserver=http.createServer((req, res)=>{
-    if(req.url=='/home'){
-        res.end('abes engineering college');
-    }
-    else if(req.url=='/aboutus'){
-        res.end('cse student');
-    }
-    else if(req.url=='/contactus'){
-        res.end('9794200186');}
-    else{
-        res.end('404 page is not found');
-    }
-})
-myserver.listenerCount(8000,()=> console.log('server is run'));
+// const http = require('http');
+
+// const myserver = http.createServer((req, res) => {
+
+//     if (req.url == '/home') {
+//         res.end('abes engineering college');
+//     }
+
+//     else if (req.url == '/aboutus') {
+//         res.end('cse student');
+//     }
+
+//     else if (req.url == '/contactus') {
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.end('<img src="https://tse4.mm.bing.net/th/id/OIP.YRLnFB2_z8EqsilbfSMoQAHaD9?pid=Api&P=0&h=180" />');
+//     }
+
+//     else {
+//         res.end('404 page is not found');
+//     }
+// });
+
+// myserver.listen(8000, () => console.log('server is running'));
